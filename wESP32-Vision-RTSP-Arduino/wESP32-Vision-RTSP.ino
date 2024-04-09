dfea2ce73d06eee8a2203b0000c3012af30b17df
Aaron Yourk <ayourk@gmail.com>
2024-04-08 21:41:45 -0600

    Initial commit of KiCAD 6.x files and related Arduino Project.

---
+++ wESP32-Vision-RTSP-Arduino/wESP32-Vision-RTSP.ino
@@ -0,0 +1,186 @@
+#include "OV2640.h"
+#include <ETH.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <WiFiClient.h>
+
+#include "OV2640Streamer.h"
+#include "CRtspSession.h"
+
+#define ENABLE_WEBSERVER
+#define ENABLE_RTSPSERVER
+
+OV2640 cam;
+
+#ifdef ENABLE_WEBSERVER
+WebServer server(80);
+#endif
+
+#ifdef ENABLE_RTSPSERVER
+WiFiServer rtspServer(8554);
+#endif
+
+
+#ifdef ENABLE_WEBSERVER
+void handle_jpg_stream(void)
+{
+    WiFiClient client = server.client();
+    String response = "HTTP/1.1 200 OK\r\n";
+    response += "Content-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n";
+    server.sendContent(response);
+
+    while (1)
+    {
+        cam.run();
+        if (!client.connected())
+            break;
+        response = "--frame\r\n";
+        response += "Content-Type: image/jpeg\r\n\r\n";
+        server.sendContent(response);
+
+        client.write((char *)cam.getfb(), cam.getSize());
+        server.sendContent("\r\n");
+        if (!client.connected())
+            break;
+    }
+}
+
+void handle_jpg(void)
+{
+    WiFiClient client = server.client();
+
+    cam.run();
+    if (!client.connected())
+    {
+        return;
+    }
+    String response = "HTTP/1.1 200 OK\r\n";
+    response += "Content-disposition: inline; filename=capture.jpg\r\n";
+    response += "Content-type: image/jpeg\r\n\r\n";
+    server.sendContent(response);
+    client.write((char *)cam.getfb(), cam.getSize());
+}
+
+void handleNotFound()
+{
+    String message = "Server is running!\n\n";
+    message += "URI: ";
+    message += server.uri();
+    message += "\nMethod: ";
+    message += (server.method() == HTTP_GET) ? "GET" : "POST";
+    message += "\nArguments: ";
+    message += server.args();
+    message += "\n";
+    server.send(200, "text/plain", message);
+}
+#endif
+
+// Ethernet event handler
+
+void handleEvent(WiFiEvent_t event)
+{
+  switch (event) {
+    case SYSTEM_EVENT_ETH_START:
+      Serial.println("ETH Started");
+      //set eth hostname here
+      ETH.setHostname("wesp32-vision");
+      break;
+    case SYSTEM_EVENT_ETH_CONNECTED:
+      Serial.println("ETH Connected");
+      break;
+    case SYSTEM_EVENT_ETH_GOT_IP:
+      Serial.print("ETH MAC: ");
+      Serial.print(ETH.macAddress());
+      Serial.print(", IPv4: ");
+      Serial.print(ETH.localIP());
+      if (ETH.fullDuplex()) {
+        Serial.print(", FULL_DUPLEX");
+      }
+      Serial.print(", ");
+      Serial.print(ETH.linkSpeed());
+      Serial.println("Mbps");
+      break;
+    case SYSTEM_EVENT_ETH_DISCONNECTED:
+      Serial.println("ETH Disconnected");
+      break;
+    case SYSTEM_EVENT_ETH_STOP:
+      Serial.println("ETH Stopped");
+      break;
+    default:
+      break;
+  }
+}
+
+void setup()
+{
+    // Start serial console
+    Serial.begin(115200);
+
+    // Attach the callback event handler
+    WiFi.onEvent(handleEvent);
+    // Start Ethernet
+    ETH.begin();
+
+    // Start the camera
+    cam.init(wesp32_vision_config);
+
+#ifdef ENABLE_WEBSERVER
+    server.on("/", HTTP_GET, handle_jpg_stream);
+    server.on("/jpg", HTTP_GET, handle_jpg);
+    server.onNotFound(handleNotFound);
+    server.begin();
+#endif
+
+#ifdef ENABLE_RTSPSERVER
+    rtspServer.begin();
+#endif
+}
+
+CStreamer *streamer;
+CRtspSession *session;
+WiFiClient client; // FIXME, support multiple clients
+
+void loop()
+{
+#ifdef ENABLE_WEBSERVER
+    server.handleClient();
+#endif
+
+#ifdef ENABLE_RTSPSERVER
+    uint32_t msecPerFrame = 100;
+    static uint32_t lastimage = millis();
+
+    // If we have an active client connection, just service that until gone
+    // (FIXME - support multiple simultaneous clients)
+    if(session) {
+        session->handleRequests(0); // we don't use a timeout here,
+        // instead we send only if we have new enough frames
+
+        uint32_t now = millis();
+        if(now > lastimage + msecPerFrame || now < lastimage) { // handle clock rollover
+            session->broadcastCurrentFrame(now);
+            lastimage = now;
+
+            // check if we are overrunning our max frame rate
+            now = millis();
+            if(now > lastimage + msecPerFrame)
+                printf("warning exceeding max frame rate of %d ms\n", now - lastimage);
+        }
+
+        if(session->m_stopped) {
+            delete session;
+            delete streamer;
+            session = NULL;
+            streamer = NULL;
+        }
+    }
+    else {
+        client = rtspServer.accept();
+
+        if(client) {
+            streamer = new OV2640Streamer(&client, cam);             // our streamer for UDP/TCP based RTP transport
+            session = new CRtspSession(&client, streamer); // our threads RTSP session and state
+        }
+    }
+#endif
+}
